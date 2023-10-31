ddd99a5e9f33a9748375d457825101660dcae3a7
krazytofayel <tofayel@krazyit.com>
2023-10-31 13:45:33 +0600

    implementing swiper slider

--- src/Components/SharedComponents/Slider/Slider.jsx
+++ src/Components/SharedComponents/Slider/Slider.jsx
@@ -1,28 +1,44 @@
 
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
+
 
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 
-import { EffectCoverflow, Pagination, Navigation } from 'swiper/modules';
-
-import slide_image_1 from '../../../assets/images/homepage/bgimg3.png';
-import slide_image_2 from '../../../assets/images/homepage/Groupimg1.png';
-import slide_image_3 from '../../../assets/images/homepage/bgimg3.png';
+//import './style.css'
 
 
-const Slider=()=> {
+
+
+
+const Slider = () => {
+
   return (
-    <div className="container">
-     
+    <div className="container mx-auto">
+
       <Swiper
-       modules={[EffectCoverflow]} effect="effect-coverflow"
+        modules={[EffectCoverflow, Pagination, Navigation]} effect="coverflow"
         grabCursor={true}
         centeredSlides={true}
         loop={true}
-        slidesPerView={'auto'}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          }
+        }}
         coverflowEffect={{
           rotate: 0,
           stretch: 0,
@@ -35,19 +51,115 @@ const Slider=()=> {
           prevEl: '.swiper-button-prev',
           clickable: true,
         }}
-        modules={[EffectCoverflow, Pagination, Navigation]}
+        // modules={[EffectCoverflow, Pagination, Navigation]}
         className="swiper_container"
+
       >
+
+
         <SwiperSlide>
-          <img src={slide_image_1} alt="slide_image" />
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+            </a>
+            <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+          </div>
+
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_2} alt="slide_image" />
+
+<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+  <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+  </svg>
+  <a href="#">
+    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+  </a>
+  <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+</div>
+
+</SwiperSlide>
+<SwiperSlide>
+
+<div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+  <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+  </svg>
+  <a href="#">
+    <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+  </a>
+  <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+</div>
+
+</SwiperSlide>
+        <SwiperSlide>
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+            </a>
+            <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+          </div>
+
         </SwiperSlide>
         <SwiperSlide>
-          <img src={slide_image_3} alt="slide_image" />
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+            </a>
+            <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+          </div>
+
         </SwiperSlide>
-      
+        <SwiperSlide>
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+            </a>
+            <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+          </div>
+
+        </SwiperSlide>
+        <SwiperSlide>
+
+          <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow">
+            <svg className="w-7 h-7 text-gray-500 mx-auto mb-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+              <path d="M18 5h-.7c.229-.467.349-.98.351-1.5a3.5 3.5 0 0 0-3.5-3.5c-1.717 0-3.215 1.2-4.331 2.481C8.4.842 6.949 0 5.5 0A3.5 3.5 0 0 0 2 3.5c.003.52.123 1.033.351 1.5H2a2 2 0 0 0-2 2v3a1 1 0 0 0 1 1h18a1 1 0 0 0 1-1V7a2 2 0 0 0-2-2ZM8.058 5H5.5a1.5 1.5 0 0 1 0-3c.9 0 2 .754 3.092 2.122-.219.337-.392.635-.534.878Zm6.1 0h-3.742c.933-1.368 2.371-3 3.739-3a1.5 1.5 0 0 1 0 3h.003ZM11 13H9v7h2v-7Zm-4 0H2v5a2 2 0 0 0 2 2h3v-7Zm6 0v7h3a2 2 0 0 0 2-2v-5h-5Z" />
+            </svg>
+            <a href="#">
+              <h5 className="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">Michel jonson</h5>
+            </a>
+            <p className="mb-3 text-start font-normal text-gray-500 ">Very comprehensive lessons, this is underrated. I hope other learners know about this.</p>
+
+          </div>
+
+        </SwiperSlide>
+
+
+
+
+
 
         <div className="slider-controler">
           <div className="swiper-button-prev slider-arrow">
