5041036fd5e1ddd826dbdf4daf6dc6d5068c4e56
krazytofayel <tofayel@krazyit.com>
2023-11-03 17:15:47 +0600

    resource  check done

--- src/Components/Pages/Resources/Resources.jsx
+++ src/Components/Pages/Resources/Resources.jsx
@@ -86,10 +86,10 @@ const Resources = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
   return (
     <div>
-      {/* <div className={`z-10 container-xl mx-auto xl:px-56 ${navfix ? ' top-0 h-[6%] w-full fixed bg-white ' : ''}`} >
+      <div className={`z-10 ${navfix ? ' top-0 h-[12%] w-full fixed bg-white ' : ''}`} >
         <Navbars className='relative ' />
 
-      </div> */}
+      </div>
 
 
 
@@ -300,8 +300,8 @@ const Resources = () => {
                                 }
                               }}
                               className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg ${currentPage === Math.ceil(Resourcescarddata.length / itemsPerPage)
-                                  ? "opacity-50 cursor-not-allowed"
-                                  : "hover:bg-gray-100 hover:text-gray-700"
+                                ? "opacity-50 cursor-not-allowed"
+                                : "hover:bg-gray-100 hover:text-gray-700"
                                 }`}
                             >
                               Next
@@ -335,20 +335,27 @@ const Resources = () => {
             </div>
 
 
-            <div>
-              <div className="flex justify-between items-center bg-[#2c6777] rounded-md p-5 mt-5 mb-5">
-                <div className="text-white">
-                  <h2 className="text-2xl font-semibold">Request the best tutor for your child</h2>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
-                </div>
-                <div>
-                  <button className="bg-white text-black text-sm font-normal px-3 py-2 rounded-full">
-                    Request a Tutor
-                  </button>
+            <section className="">
+              <div className="container px-5 py-24 mx-auto">
+                <div className="bg-[#2C6777] py-10 rounded-3xl ">
+                  <div className="lg:w-full flex items-center flex-col sm:flex-row text-center lg:text-start md:text-start mx-auto lg:px-10 px-4 ">
+                    <div className="lg:w-3/4 md:3/5">
+                      <h1 className="flex-grow sm:pr-16 text-2xl font-medium title-font text-white ">Create Tutor2u account now</h1>
+                      <p className="flex-grow sm:pr-16 text-xl font-medium title-font text-white">Join us and start your journey towards excellence.</p>
+                    </div>
+                    <div className="lg:w-1/4 md:2/5">
+                      <button className="flex-shrink-0 text-[#21515e] font-semibold bg-white border-0 py-2 lg:px-8 md:px-5 px-8  hover:bg-[#5cdcff]  rounded-3xl text-lg mt-10 sm:mt-0">Request a Tutor</button>
+
+                    </div>
+
+                  </div>
+
                 </div>
 
+
               </div>
-            </div>
+
+            </section>
           </div>
         </div>
       </section>
