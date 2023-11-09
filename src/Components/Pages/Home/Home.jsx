55e2a17b7eac5fa8ff664459fc74f30d0a325a7e
krazytofayel <tofayel@krazyit.com>
2023-11-09 14:28:18 +0600

    try to change home banner

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Navbars from "../../SharedComponents/Navbar/Navbars"
 import Footer from "../../SharedComponents/Footer/Footer";
-import homebg from '../../../assets/images/homepage/HomeImage.png'
+import homebg from '../../../assets/images/homepage/Maskgroup.png'
 import homebg3 from '../../../assets/images/homepage/bgimg3.png'
 import Carddata from '../../SharedComponents/Data/HomeSubjectCarddata/homesubjectcarddata.json'
 import ExploreTutordata from '../../SharedComponents/Data/ExploreTutordata/ExploreTutordata.json'
@@ -37,7 +37,7 @@ const Home = () => {
 
 
 
-      <section className="Home_Top_Banner bg-[#2C6777]">
+      {/* <section className="Home_Top_Banner bg-[#2C6777]">
         <div className="container mx-auto pb-0 md:pb-0 lg:pb-0 px-2 md:px-0 lg:px-0  py-10 lg:py-14 md:py-10 xl:pt-10  ">
           <div className="mx-auto flex flex-wrap  md:flex-row flex-col items-center">
             <div className="xl:w-3/6 lg:w-2/4 md:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0 md:pr-5">
@@ -72,6 +72,44 @@ const Home = () => {
               <img alt="Top-Banner-Side-Image" className=" " src={homebg} />
             </div>
           </div>
+        </div>
+      </section> */}
+{/* try another way  */}
+      <section className="Home_Top_Banner bg-[#dcecfa]">
+
+        <div className="container mx-auto flex   md:flex-row flex-col items-center">
+          <div className="  w-full ">
+            <div className=" ">
+              <h1 className="text-[#2c6777] text-center md:text-start lg:text-start text-[24px] xl:text-[50px] lg:text-[36px] md:text-[27px] title-font font-bold mb-4 ">
+                We help you find <br /> and manage tutors.
+              </h1>
+
+              <p className="leading-relaxed text-center md:text-start lg:text-start xl:text-[24px] text-lg text-[#2c6777] mb-4 lg:font-semibold md:font-semibold font-normal">
+                Skilled and Emotionally Intelligent Educators that will make learning exciting and engaging.
+              </p>
+              <div>
+
+                <form className="max-w-xl mt-10 p-2 md:p-0 lg:p-0">
+                  <label className=" text-sm font-medium text-gray-900 sr-only ">Search</label>
+                  <div className="relative">
+                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 mt-1 pointer-events-none">
+                      <svg className="w-4 h-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                      </svg>
+                    </div>
+                    <input type="search" id="default-search" className="block w-full p-3 pl-10 text-sm text-gray-900 border border-gray-300 rounded-full bg-white focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos..." required />
+                    <button type="submit" className="text-white absolute right-2 bottom-[5px] bg-[#2c6777]  focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-4 py-2 ">Search</button>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+
+          <div className=" w-full  object-cover flex justify-end">
+            <img alt="Top-Banner-Side-Image" className="" src={homebg} />
+          </div>
+
         </div>
       </section>
 
