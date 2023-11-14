3bf9439a684820a19ed6f208a8ccd644127a4541
krazytofayel <tofayel@krazyit.com>
2023-11-14 12:47:03 +0600

    tution job details  page check complete

--- src/Components/Pages/TutionJobDetails/TutionJobDetails.jsx
+++ src/Components/Pages/TutionJobDetails/TutionJobDetails.jsx
@@ -26,7 +26,7 @@ const TutionJobDetails = () => {
 
     return (
         <>
-          <div className={`z-10  ${navfix ? ' top-0 h-[9%] w-full fixed bg-white transition-all duration-300 ease-in-out ' : ''}`} >
+       <div className={`z-20  ${navfix ? ' top-0 h-[9%] md:h-[11.5%] lg:h-[11%] w-full fixed bg-white transition-all duration-300 ease-in-out ' : ''}`} >
         <Navbars className='relative ' />
 
       </div>
