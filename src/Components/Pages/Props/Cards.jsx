139f3e5ae164fd4d538220aa0e1a60f205be98fe
krazytofayel <tofayel@krazyit.com>
2023-11-20 10:10:58 +0600

    (feat):homepage main section ,card tutorprofile section updated

--- src/Components/Pages/Props/Cards.jsx
+++ src/Components/Pages/Props/Cards.jsx
@@ -1,18 +1,16 @@
-
-
 const Cards = ({ carditem }) => {
   const { id, image,subject } = carditem
   //console.log(carditem.length)
   return (
     <>
 
-      <div className="w-full  shadow-lg border border-[#089bab] p-2 rounded-lg">
+      <div className="w-full  shadow-xl border  p-2 rounded-lg">
         <div className="flex gap-5 items-center ">
-          <div >
+          <div  className="rounded-md border bg-[#f3f4f6] shadow-lg">
             <img className="h-20 w-20 rounded " src={image } alt="404" />
           </div>
           <div>
-            <p className="font-bold">{subject}</p>
+            <p className="font-bold  text-gray-800">{subject}</p>
           </div>
         </div>
 
